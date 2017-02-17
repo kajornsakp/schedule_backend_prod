1bd2f74acfd77eac1e9e23fb1b9f626c92463842
Patipon Riebpradit <patipon74@gmail.com>
2017-02-17 11:46:23 +0700

    init project

---
+++ src/main/java/com/example/IcSparkBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcSparkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcSparkBackendApplication.class, args);
+	}
+}
