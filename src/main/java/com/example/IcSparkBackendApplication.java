4075022b2ed7a94eca2379d287b143d14eedfa50
Patipon Riebpradit <patipon74@gmail.com>
2017-03-10 15:55:32 +0700

    add unique id generator in subject

--- src/main/java/com/example/IcSparkBackendApplication.java
+++ src/main/java/com/example/IcSparkBackendApplication.java
@@ -3,10 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class IcSparkBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IcSparkBackendApplication.class, args);
+
 	}
 }
--- src/main/java/model/Subject.java
+++ src/main/java/model/Subject.java
@@ -1,16 +1,43 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.UUID;
+
 
 public class Subject {
     private String name;
     private ArrayList<Lecturer> lecturerList;
     private String dayTimePrefer;
+    private String id;
+    private int expectedNumber;
 
     public Subject(String name){
         this.name = name;
         this.lecturerList = new ArrayList<Lecturer>();
+        this.id = this.generateID();
     }
+
+    private String generateID(){
+        UUID tempID = UUID.randomUUID();
+        String[] parts = tempID.toString().split("-");
+        String result = "";
+        for (int i = 0 ; i < 4 ; i++){
+            int a = (parts[1].charAt(i) + parts[2].charAt(i)) - 30;
+            result += (char) a;
+
+        }
+        return result;
+    }
+
+    public void setID(String number){
+        this.id = number;
+    }
+
+    public void setExpectedNumber(int number){
+        this.expectedNumber = number;
+    }
+
+
 
     public void addLecturer(Lecturer l){
         this.lecturerList.add(l);
