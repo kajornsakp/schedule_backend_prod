c38d31a43fc16994bfc9eafc1c03e75180d56205
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2017-05-20 17:19:03 +0700

    added mongo remote config

--- src/main/java/com/example/config/MongoConfig.java
+++ src/main/java/com/example/config/MongoConfig.java
@@ -2,6 +2,8 @@ package com.example.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
@@ -10,13 +12,21 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import java.util.Arrays;
+
 
 @Configuration
 @EnableMongoRepositories
 public class MongoConfig{
     public @Bean
     MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory(new MongoClient(), "ICSpark");
+//        return new SimpleMongoDbFactory(new MongoClient(), "ICSpark");
+        MongoCredential credential = MongoCredential.createCredential("schedule","schedule","schedule".toCharArray());
+        ServerAddress serverAddress = new ServerAddress("188.166.187.184",27017);
+
+        MongoClient mongoClient = new MongoClient(serverAddress, Arrays.asList(credential));
+        SimpleMongoDbFactory simpleMongoDbFactory = new SimpleMongoDbFactory(mongoClient,"schedule");
+        return simpleMongoDbFactory;
     }
 
     @Bean
@@ -35,4 +45,5 @@ public class MongoConfig{
 
     }
 }
+
  
