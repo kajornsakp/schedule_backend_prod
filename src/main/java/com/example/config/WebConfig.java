e4c18e51c7ea2ec48a82bb163a3e3e0a1046dde2
Patipon Riebpradit <patipon74@gmail.com>
2017-05-22 12:31:14 +0700

    add requestMatchers for CORS request

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedMethods("GET","POST","PUT","DELETE");
     }
 }
