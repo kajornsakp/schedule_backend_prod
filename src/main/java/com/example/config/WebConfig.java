cae249fcef3af4bf1edb92667c44c3b45dad664a
Patipon Riebpradit <patipon74@gmail.com>
2017-05-22 12:54:23 +0700

    Fix 2 : create CORS filter to allow access control allow origin

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
+@ComponentScan("com.example.config")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
