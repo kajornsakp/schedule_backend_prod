9ba0310f6b5ff3f71f252cb77f6568e873fdbef6
Patipon Riebpradit <patipon74@gmail.com>
2017-05-22 23:06:17 +0700

    attempt 1 : try add option handler in global controller

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators.Add;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -16,6 +17,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
+    	System.out.println("CORS MAPPPING ADDED");
         registry.addMapping("/**").allowedOrigins("*");
     }
 }
