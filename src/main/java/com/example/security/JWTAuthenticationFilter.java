e2615fe5f13952bab17f4d34e4c8c37055c56abe
Patipon Riebpradit <patipon74@gmail.com>
2017-05-23 00:59:01 +0700

    rewrited request header allow origin

--- src/main/java/com/example/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/security/JWTAuthenticationFilter.java
@@ -24,7 +24,7 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
 		System.out.println("get authentication filter with request : " + request);
 		HttpServletResponse res = (HttpServletResponse) response;
 	    res.setHeader("Access-Control-Allow-Origin", "*");
-	    res.setHeader("Access-Control-Allow-Headers", "*");
+	    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		//just to print check
 		Enumeration params = request.getParameterNames(); 
 		while(params.hasMoreElements()){
